a4b39b26199dc84e68e20c2458da2a25351ed696
Yusuke Sato <yususato@c1r4s8.42tokyo.jp>
2024-07-12 22:19:01 +0900

    first commit

---
+++ Makefile
@@ -0,0 +1,34 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    Makefile                                           :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: yususato <yususato@student.42.fr>          +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2024/07/12 21:36:18 by yususato          #+#    #+#              #
+#    Updated: 2024/07/12 22:00:53 by yususato         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+NAME = philo
+CC = cc
+INCLUDES_DIR = ./includes
+CFLAGS = -Wall -Wextra  -Werror -I$(INCLUDES_DIR)
+SRCS = philo.c
+
+OBJS = $(SRCS:.c=.o)
+
+all : $(NAME)
+
+$(NAME): $(OBJS)
+	$(CC) $(CFLAGS) -o $(NAME) $(OBJS)
+
+clean:
+	$(RM) $(OBJS)
+
+fclean: clean
+	$(RM) $(NAME)
+
+re: fclean all
+
+.PHONY: all clean fclean re
---
+++ philo.h
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yususato <yususato@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/12 21:33:45 by yususato          #+#    #+#             */
+/*   Updated: 2024/07/12 22:14:14 by yususato         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <pthread.h>
+# include <errno.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <stdbool.h>
+# include <limits.h>
+# include <string.h>
+# include <signal.h>
+# include <fcntl.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+
+typedef struct s_philo
+{
+	pthread_t	thread;
+	int			id;
+	int			eating;
+} t_philo;
+
+#endif
